838720c3e45afcbd85531f181683efa81c4decf2
paperclipstudio <paperclipstudio@gmail.com>
2020-12-08 15:16:39 +0000

    Refactor

--- Day8/part1.hs
+++ Day8/part1.hs
@@ -2,20 +2,23 @@
 run file = do
     handle <- readFile file
     let instructions = lines handle
-        out = boot instructions 0 0 []
+        out = runProgram (State instructions 0 0) []
     print out
 
 
 split line = (take 3 line, read (drop 5 line) * (if line !! 4 == '+' then 1 else (-1)))
-boot lines lineNum acc run
-    | lineNum `elem` run = acc
-    | length lines <= lineNum = acc
-    | instruction == "acc" = boot lines (lineNum + 1) (acc + value) (lineNum:run)
-    | instruction == "jmp" = boot lines (lineNum + value) acc (lineNum:run)
-    | instruction == "nop" = boot lines (lineNum + 1) acc (lineNum:run)
-    | otherwise = acc
+
+data CPUState = State [String] Int Int
+clock :: CPUState -> CPUState
+clock (State lines lineNum acc)
+    | instruction == "acc" = State lines (lineNum + 1) (acc + value) 
+    | instruction == "jmp" = State lines (lineNum + value) acc
+    | otherwise            = State lines (lineNum + 1) acc 
     where
         (instruction, value) = split $ lines !! lineNum
 
-
-        
+runProgram :: CPUState -> [Int] -> Int
+runProgram state@(State lines lineNum acc) linesRun 
+    | lineNum `elem` linesRun = acc
+    | lineNum >= length lines = acc
+    | otherwise = runProgram (clock state) (lineNum:linesRun)
