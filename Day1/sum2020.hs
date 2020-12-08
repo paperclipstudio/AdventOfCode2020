9242d6ebd3a37f9e5d9d2f5d5033feddabb13273
paperclipstudio <paperclipstudio@gmail.com>
2020-12-08 11:43:41 +0000

    Day1 part 2

--- Day1/sum2020.hs
+++ Day1/sum2020.hs
@@ -6,10 +6,21 @@ sum2020 (x:xs)
     | (2020 - x)  `elem` xs = x * (2020-x)
     | otherwise =  sum2020 xs
 
-main = do
+
+main = apply $  threeSum2020
+
+apply func = do
     handle <- readFile "data.txt"
     let line = lines handle
         nums = map read line :: [Int]
-    putStr $ show $ sum2020 nums
+    putStr $ show $ func nums
 
-    
+-- Part 2
+sumGen _ [] = 0
+sumGen target (x:xs)
+    | (target - x) `elem` xs = x * (target - x)
+    | otherwise = sumGen target xs   
+
+threeSum2020 (x:xs) =
+    let result =  x * sumGen (2020 - x) xs in
+    if result /= 0 then result else threeSum2020 xs
